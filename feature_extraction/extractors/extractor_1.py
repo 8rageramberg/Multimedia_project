cd3af856bdddce3ad14f68c1bf3cc1a164a8aadf
yungtobyy <thus0518@uni.sydney.edu.au>
2024-05-01 12:17:46 +1000

    change url --> path in all files

--- feature_extraction/extractors/extractor_1.py
+++ feature_extraction/extractors/extractor_1.py
@@ -2,21 +2,21 @@
 Class representing an Extractor 1.
 
 Attributes:
-    - photo_url (Str): URL of current photo
+    - photo_path (Str): path of current photo
 '''
 class extractor_1:
     name = "Extractor 1"
-    photo_url = None
+    photo_path = None
 
-    def __init__(self, photo_url=""):
-        self.photo_url = photo_url
+    def __init__(self, photo_path=""):
+        self.photo_path = photo_path
         
     '''
     Main func:
     '''
     def get_features(self):
         # TODO: Implement a feature extraction algo.
-        # For example: with open(self.photo_url): *some math*
+        # For example: with open(self.photo_path): *some math*
 
         return None
     
--- feature_extraction/feature_extractor.py
+++ feature_extraction/feature_extractor.py
@@ -8,7 +8,7 @@ A class reperesenting a feature extractor. The feature extractor has
 different extractor objects for specific extracting purposes.
 
 Attributes:
-    - photo_url (Str): URL of current photo
+    - photo_path (Str): path of current photo
     - edge_detector (edge_detecor): Edge detector object
     - list_of_extractor (List[extractors]): List of all extractors
 
@@ -16,14 +16,14 @@ Functions:
     - extract():              Function for extracting each feature of an image
                               utilizing all extractors.
 
-    - set_photo_url(new_url): Function for setting the photo_url
+    - set_photo_path(new_path): Function for setting the photo_path
 
-    - get_photo_url():        Function for retrieving the photo_url
+    - get_photo_path():        Function for retrieving the photo_path
 '''
 class feature_extractor:
-    # Class variables for storing photo url, feature_extractors
+    # Class variables for storing photo path, feature_extractors
     # and all extractors in a list for future use.
-    photo_url = None
+    photo_path = None
 
     edge_detector = None
     extractor_1 = None
@@ -33,14 +33,14 @@ class feature_extractor:
 
 
     # Initiate the feature extractor
-    def __init__(self, photo_url=""):
-        # Set the url of the photo
-        self.photo_url = photo_url
+    def __init__(self, photo_path=""):
+        # Set the path of the photo
+        self.photo_path = photo_path
 
         # TODO: Add more features
-        self.edge_detector = edge_detector(photo_url)
-        self.extractor_1 = extractor_1(photo_url)
-        self.extractor_2 = extractor_2(photo_url)
+        self.edge_detector = edge_detector(photo_path)
+        self.extractor_1 = extractor_1(photo_path)
+        self.extractor_2 = extractor_2(photo_path)
 
         # TODO: For every feature added to list of feature extractors
         self.list_of_feature_extractors.append(self.edge_detector)
@@ -60,11 +60,11 @@ class feature_extractor:
         return features
 
 
-    '''Setter function for setting photo url'''
-    def set_photo_url(self, new_url):
-        self.photo_url = new_url
+    '''Setter function for setting photo path'''
+    def set_photo_path(self, new_path):
+        self.photo_path = new_path
 
 
-    '''Getter function for getting photo url'''
-    def get_photo_url(self):
-        return self.photo_url
+    '''Getter function for getting photo path'''
+    def get_photo_path(self):
+        return self.photo_path
