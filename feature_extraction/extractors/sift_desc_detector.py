cb21a721471a54f64ef6c4b1c39a68b5a9cc9145
yungtobyy <tooobbbi.urmaker@gmail.com>
2024-05-03 13:17:05 +1000

    added sift_desc_detector.py

--- feature_extraction/extractors/sift_desc_detector.py
+++ feature_extraction/extractors/sift_desc_detector.py
@@ -1,11 +1,14 @@
 '''
-Class representing an Extractor 1.
+Class representing a SIFT (Scale invariant feature transform)
+descriptor detector which automatically detects the 20 most
+representative descriptors via PCA (Principal component analysis)
+and returns the as a list of vectors.
 
 Attributes:
     - photo_path (Str): path of current photo
 '''
-class extractor_1:
-    name = "Extractor 1"
+class sift_desc_detector:
+    name = "SIFT descriptor detector"
     photo_path = None
 
     def __init__(self, photo_path=""):
