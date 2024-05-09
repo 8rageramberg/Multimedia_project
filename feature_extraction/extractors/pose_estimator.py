8597cf041f8a6c583d434af20b87471f63317ee1
yungtoby <tooobbbi.urmaker@gmail.com>
2024-05-09 16:49:26 +1000

    pose_estimator.py cleanup

--- feature_extraction/extractors/pose_estimator.py
+++ feature_extraction/extractors/pose_estimator.py
@@ -21,15 +21,27 @@ from scipy.spatial.distance import euclidean
 # MAC: curl -o pose_landmarker.task -O https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_heavy/float16/1/pose_landmarker_heavy.task
 
 # https://github.com/google/mediapipe/blob/master/docs/solutions/pose.md
-'''
-Class representing pose estimation.
 
-Attributes:
-    - photo_path (Str): path of current photo
-'''
 class pose_estimator:
+    '''
+    Class representing a Pose estimator. The Pose estimator classes
+    utilizes google's MediaPipe framework to compose a pose from a
+    input image and features to retrieve this and compare. 
+
+    Attributes:
+        - photo_path (Str): Path of current photo
+        - model_path (Str)
+
+    Functions:
+        - get_features(): Retrieve the pose features
+
+        - compare(): Compare to poses against eachother
+                    and retrieve a mean similarity score.
+
+    '''
     name = "Pose_estimator"
-    photo_path = None 
+    photo_path = None
+    own_keypoints = None
     
     def __init__(self, photo_path="", model_path = 'pose_landmarker_heavy'):
         self.model_path = model_path
@@ -47,10 +59,24 @@ class pose_estimator:
         )
 
         self.scaler = MinMaxScaler(feature_range=(0, 1))
-        
+    
+
+
     def get_features(self, display=False):
-        image = cv2.imread(self.photo_path)                 # Read input image
-        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Cast to RGB
+        '''
+        Function to retrieve features in the sense of keypoints
+        to the pose of the image currently attached. 
+
+        Parameters:
+            - display (Boolean):              To display the features after retrieval
+
+        Returns:
+            - keypoints (List[keypoint obj]): A list of keypoint objects representing
+                                              the current image pose
+        '''
+        # Read input image and project onto other color space
+        image = cv2.imread(self.photo_path)
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
 
         # Process the image and get pose landmarks
         results = self.pose.process(image_rgb)
@@ -66,37 +92,58 @@ class pose_estimator:
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
             
-            keypoints = []                                              # martiniblack: https://github.com/google/mediapipe/issues/1020
-            for feature in results.pose_landmarks.landmark:             # How to extract the coordinates (keypoints) from a landmark object
+            # How to extract the coordinates (keypoints) from a landmark object
+            # martiniblack: https://github.com/google/mediapipe/issues/1020
+            keypoints = []                                              
+            for feature in results.pose_landmarks.landmark:
                 keypoints.append({
                                     'X': feature.x,
                                     'Y': feature.y,
                                     'Z': feature.z,
                                     'Visibility': feature.visibility,
                                     })
+            self.own_keypoints = keypoints
             return keypoints
         else:
             #print("Something wrong with landmarks, features not retrieved")
             return None
     
-    # Compare compares two keypoint lists
-    def compare(self, keypoints_1, keypoints_2):
-        if keypoints_1 is None or keypoints_2 is None:              # Error handeling, if landmarks on image did not exist None is returned, 
-            return 0                                                # if None, there is a 0% match
+    
+
+    def compare(self, keypoints_to_compare):
+        '''
+        Function for comparing own keypoints agains other keypoint lists
+
+        Parameters:
+            - keypoints_to_compare (List[keypoints obj]): List of keypoints to compare against
+                                                          it's own.
+
+        Returns:
+            - mean_similarity_score (Float[0-100]):       A mean similarity score float
+        '''
+        # Error handeling, if landmarks on image did not exist None is returned,
+        # if None, there is a 0% match
+        if self.own_keypoints is None or keypoints_to_compare is None: 
+            return 0
         
+        # Loop keypoints and do euclidean distance for each row
         euclidean_dist = []
-        for kp1, kp2 in zip(keypoints_1, keypoints_2):              # Loop keypoints and do euclidean distance for each row
+        for kp1, kp2 in zip(self.own_keypoints, keypoints_to_compare):
             features_1 = np.array([kp1['X'], kp1['Y'], kp1['Z'], kp1['Visibility']])
             features_2 = np.array([kp2['X'], kp2['Y'], kp2['Z'], kp2['Visibility']])
             euclidean_dist.append(euclidean(features_1, features_2))
         
+        # Flip so lower distances is higher similarity, then scale the similarity
+        # score on a 1-100 range, and retrieve the mean score
         euclidean_dist_reshape = np.array(euclidean_dist).reshape(-1, 1)
         scaled_dist = self.scaler.fit_transform(euclidean_dist_reshape)
-        similarity_scores = 1 - scaled_dist                         # Flip so lower distances is higher similarity
-        scaled_similarity_scores = similarity_scores * 100          # Scale the similarity scores to the 1-100 range
-        mean_similarity_score = np.mean(scaled_similarity_scores)   # Mean of scores
+        similarity_scores = 1 - scaled_dist
+        scaled_similarity_scores = similarity_scores * 100
+        mean_similarity_score = np.mean(scaled_similarity_scores)   
         return mean_similarity_score
         
+
+
     def get_name(self):
         '''Getter function for getting extractor name'''
         return self.name
