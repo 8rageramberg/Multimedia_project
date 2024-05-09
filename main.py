1e6efdde545f41d20d9c6de005f3eee9dd6c9357
8rageramberg <brage.ramberg@gmail.com>
2024-05-09 18:55:32 +1000

    changes to web server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Imports
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request
 
 
 ##### RUN TO START APPLICATION: #####
@@ -7,9 +8,21 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static')
 @app.route('/')
 
-
 def index():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def save_image():
+    if request.method == 'POST':
+            image = request.files['image']
+            if image:
+                # Save the uploaded file to the static directory
+                filename = 'uploaded_image.jpg'
+                filepath = 'static'
+                image.save(os.path.join(filepath, filename))
+                return render_template('index.html', filename=filename)
+            else:
+                return 'No image provided in the request', render_template('index.html')
 
 def run_app():
     app.run(port=8000, debug=True) # If needed you can change port here
