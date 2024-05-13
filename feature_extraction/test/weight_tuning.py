ab415457b9c28169b436fe10601e50632884f3a3
seer33 <se.eroglu34@gmail.com>
2024-05-13 14:58:38 +1000

    edit tuning

--- feature_extraction/test/weight_tuning.py
+++ feature_extraction/test/weight_tuning.py
@@ -18,77 +18,112 @@ class FeatureWeightOptimizer:
     def __init__(self, feature_extractor):
         self.feature_extractor = feature_extractor
     
-    def calculate_distance(self, path_1, path_2, w1, w2, w3):
+    def calculate_score(self, path_1, path_2, w1, w2, w3):
         """
-        Calculate the distance between two images.
+        Calculate the score between two images.
 
         Args:
             full_path (str): The path to the first image.
             other_full_path (str): The path to the second image.
 
         Returns:
-            float: The distance between the two images.
+            float: The score between the two images.
         """
-        #compute distance 1
+        #compute pose estimator score1
         
-        data = pd.read_csv("pose_estimator_features", sep="|") 
+        data = pd.read_csv("pose_estimator_features.pkl") 
 
         # Search for the rows corresponding to the image paths
         row_1 = data[data['photo_path'] == path_1]
         row_2 = data[data['photo_path'] == path_2]
 
         # Extract the feature vectors (assuming they are stored as strings)
-        features_1 = row_1['features'].iloc[0].strip('[]').split(', ')  # Remove brackets and split into numbers
-        features_2 = row_2['features'].iloc[0].strip('[]').split(', ')
+        features_1 = row_1['features']  # Remove brackets and split into numbers
+        features_2 = row_2['features']
 
         # Convert feature vectors to numeric format
-        features_1 = [float(x) for x in features_1]
-        features_2 = [float(x) for x in features_2]
+        # features_1 = [float(x) for x in features_1]
+        # features_2 = [float(x) for x in features_2]
 
-        # Calculate the Euclidean distance
-        distance1 = distance.euclidean(features_1, features_2)        
+        
+        if features_1 is None or features_2 is None: 
+            return 0
+        
+        # Loop keypoints and do euclidean distance for each row
+        euclidean_dist = []
+        for kp1, kp2 in zip(keypoints1, keypoints2):
+            euclidean_dist.append(euclidean(kp1, kp2))
+
+        # Flip so lower distances is higher similarity, then scale the similarity
+        # score on a 1-100 range, and retrieve the mean score
+        euclidean_dist_reshape = np.array(euclidean_dist).reshape(-1, 1)
+       
+        mean_eculidean = 1 - np.mean(euclidean_dist_reshape)/2.4494897428   
+        mean_similarity_score = 100*(mean_eculidean)
+
+        #similarity_scores = 100 - scaled_dist
+        score1 = mean_similarity_score 
 
         #TODO: compute distance 2 and 3
-        data = pd.read_csv("SIFT_descriptor_detector_features", sep="|") 
+        data = pd.read_csv("SIFT_descriptor_detector_features.pkl") 
 
         # Search for the rows corresponding to the image paths
         row_1 = data[data['photo_path'] == path_1]
         row_2 = data[data['photo_path'] == path_2]
 
         # Extract the feature vectors (assuming they are stored as strings)
-        features_1 = row_1['features'].iloc[0].strip('[]').split(', ')  # Remove brackets and split into numbers
-        features_2 = row_2['features'].iloc[0].strip('[]').split(', ')
+        features_1 = row_1['features']  # Remove brackets and split into numbers
+        features_2 = row_2['features']
 
         # Convert feature vectors to numeric format
-        features_1 = [float(x) for x in features_1]
-        features_2 = [float(x) for x in features_2]
+        # features_1 = [float(x) for x in features_1]
+        # features_2 = [float(x) for x in features_2]
 
-        # Calculate the Euclidean distance
-        distance2 = distance.euclidean(features_1, features_2)  
+        # Calculate the SIFT score2
+        # Utilizing the cv2 libraries brute force matcher
+        brute_force_matcher = cv.BFMatcher()
+        matches = brute_force_matcher.knnMatch(features_1, features_2, 2)
 
-        data = pd.read_csv("pose_estimator_features", sep="|")  # Replace 'your_data.csv' with your actual file name
+        # Applying a ratio test (from: https://docs.opencv.org/4.x/dc/dc3/tutorial_py_matcher.html)
+        good_matches = []
+        good_match_ratio = 0.75	
+        for neighbor_1, neighbor_2 in matches:
+            if (neighbor_1.distance < good_match_ratio * neighbor_2.distance):
+                good_matches.append([neighbor_1])
+
+        # Calculate metric:
+        num_good_matches = len(good_matches)
+        if num_good_matches == 0:
+            score2 = 0
+        else:
+            score2 = (num_good_matches / self.nr_descriptors) * 100
+
+
+
+        data = pd.read_csv("CNN_features.pkl")  # Replace 'your_data.csv' with your actual file name
 
         # Search for the rows corresponding to the image paths
         row_1 = data[data['photo_path'] == path_1]
         row_2 = data[data['photo_path'] == path_2]
 
         # Extract the feature vectors (assuming they are stored as strings)
-        features_1 = row_1['features'].iloc[0].strip('[]').split(', ')  # Remove brackets and split into numbers
-        features_2 = row_2['features'].iloc[0].strip('[]').split(', ')
+        features_1 = row_1['features'] 
+        features_2 = row_2['features']
 
         # Convert feature vectors to numeric format
-        features_1 = [float(x) for x in features_1]
-        features_2 = [float(x) for x in features_2]
+        # features_1 = [float(x) for x in features_1]
+        # features_2 = [float(x) for x in features_2]
 
-        # Calculate the Euclidean distance
-        distance3 = distance.euclidean(features_1, features_2)  
+        # Calculate the CNN score3        
+        distance = np.linalg.norm((self.features - features_to_compare) / 2500 * 100)
+        score3 =  100 - (distance / 5000) * 100
         
 
 
         # Compute weighted sum of euclidean distances
-        weighted_distance = w1 * distance1 + w2 * distance2 + w3 * distance3
+        weighted_score = w1 * score1 + w2 * score2 + w3 * score3
 
-        return weighted_distance
+        return weighted_score
     
     def extract_subfolder(self, path):
         parts = path.split('/')  # Split the path into segments
@@ -127,16 +162,16 @@ class FeatureWeightOptimizer:
                         relative_path = os.path.join('archive', os.path.relpath(os.path.join(subdir, file), archive_path))  # Get relative path
                         # TODO: find closest image among the other image, i.e. exclude the image itself in the comparison
                         closest_image = ""
-                        closest_distance = float('inf')
+                        best_score = float('inf')
                         for other_subdir, other_dirs, other_files in os.walk(archive_path):
                             for other_file in other_files:
                                 if other_file.lower().endswith(('.png', '.jpg', '.jpeg')):  # Check for image files
                                     other_relative_path = os.path.join('archive', os.path.relpath(os.path.join(other_subdir, other_file), archive_path))
                                     if other_relative_path != relative_path:  # Exclude the image itself
-                                        # TODO: Calculate distance between images                                        
-                                        distance = self.calculate_distance(relative_path, other_relative_path, test_weights[0], test_weights[1], test_weights[2])
-                                        if distance < closest_distance:
-                                            closest_distance = distance
+                                        # TODO: Calculate score between images                                        
+                                        score = self.calculate_score(relative_path, other_relative_path, test_weights[0], test_weights[1], test_weights[2])
+                                        if score > best_score:
+                                            best_score = score
                                             closest_image = other_relative_path
 
                         if closest_image != "":
@@ -151,5 +186,3 @@ class FeatureWeightOptimizer:
         print('best_weights are ' + str(best_weights))
         
         return best_weights
-    
-    
