202cab6094d0c273e96e25e2c29df4f34c55b5ad
seer33 <se.eroglu34@gmail.com>
2024-05-13 21:16:31 +1000

    njfxkj

--- weight_tuning.py
+++ weight_tuning.py
@@ -37,7 +37,9 @@ class FeatureWeightOptimizer:
         
     
     def calculate_score(self, path_1, path_2, w1, w2, w3):
+        print(Fore.BLUE + f'Calculating score between {path_1} and {path_2}' + Fore.RESET)
         """
+
         Calculate the score between two images.
 
         Args:
